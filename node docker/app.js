3de5a0d82188212b944fc86c6453fe53510c1b70
Surajit Karuri <surajitkaruri@gmail.com>
2022-03-08 11:01:45 +0530

    added new route

--- node docker/app.js
+++ node docker/app.js
@@ -22,6 +22,11 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
+app.get('/greet',(req, res)=>{
+  const {fname, lname} = req.query
+  res.send(`Hello ${fname} ${lname}`)
+})
+
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
